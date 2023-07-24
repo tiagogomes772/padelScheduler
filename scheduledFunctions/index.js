193f81c6dce5dfd126e9eef7b613eb19f099ef44
Tiago Gomes <tiagogomes772@gmail.com>
2023-07-24 17:36:06 +0100

    Update index.js

--- scheduledFunctions/index.js
+++ scheduledFunctions/index.js
@@ -1,10 +1,12 @@
 import CronJob from "node-cron";
+import { getNextMonday, getNextSunday } from "../utils/index.js";
 
 const CHANNEL_ID = "@testbotportugal";
 
-export const initScheduledJobs = (bot, nextWeekDate, nextWeekEndDate) => {
+export const initScheduledJobs = (bot) => {
   const scheduledJobFunction = CronJob.schedule("*/1 * * * *", () => {
-    console.log("HERE12");
+    const nextWeekDate = getNextMonday();
+    const nextWeekEndDate = getNextSunday(nextWeekDate);
     bot.telegram.sendPoll(CHANNEL_ID, `Que dia da semana vamos jogar padel ${nextWeekDate.getDate()}/${nextWeekDate.getMonth()} to ${nextWeekEndDate.getDate()}/${nextWeekEndDate.getMonth()}`, ["Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sabado", "Domingo"], { allows_multiple_answers: true, is_anonymous: false})
   });
   scheduledJobFunction.start();
