cdcb396f060fe307147d76643178e9bd3ba443af
Tiago Gomes <tiagogomes772@gmail.com>
2023-07-24 17:35:48 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ import {} from 'dotenv/config'
 import express from 'express';
 import { initScheduledJobs } from "./scheduledFunctions/index.js";
 import { Telegraf } from "telegraf";
-import { getNextMonday, getNextSunday } from "./utils/index.js";
 
 const expressApp = express();
 
@@ -12,8 +11,7 @@ expressApp.use(express.json());
 
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
-const nextWeekDate = getNextMonday();
-const nextWeekEndDate = getNextSunday(nextWeekDate);
+
 bot.launch()
 
-initScheduledJobs(bot, nextWeekDate, nextWeekEndDate);
+initScheduledJobs(bot);
