a01dee00b1dd898b32f89f1efc270278f229fcfe
Tiago Gomes <tiagogomes772@gmail.com>
2024-12-30 16:37:00 +0000

    chore: added git ignore and month

--- scheduledFunctions/index.js
+++ scheduledFunctions/index.js
@@ -4,10 +4,7 @@ import { getNextMonday, getNextSunday } from "../utils/index.js";
 const CHANNEL_ID = "@testbotportugal";
 
 export const initScheduledJobs = (bot) => {
-  const scheduledJobFunction = CronJob.schedule("00 20 * * Mon", () => {
-    const nextWeekDate = getNextMonday();
+  const nextWeekDate = getNextMonday();
     const nextWeekEndDate = getNextSunday(nextWeekDate);
-    bot.telegram.sendPoll(CHANNEL_ID, `Que dia da semana vamos jogar padel ${nextWeekDate.getDate()}/${nextWeekDate.getMonth()} to ${nextWeekEndDate.getDate()}/${nextWeekEndDate.getMonth()}`, ["Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sabado", "Domingo"], { allows_multiple_answers: true, is_anonymous: false})
-  });
-  scheduledJobFunction.start();
+    bot.telegram.sendPoll(CHANNEL_ID, `Que dia da semana vamos jogar ${nextWeekDate.getDate()}/${nextWeekDate.toLocaleString('default', { month: 'long' })} to ${nextWeekEndDate.getDate()}/${nextWeekEndDate.toLocaleString('default', { month: 'long' })}`, ["Nao posso", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sabado", "Domingo"], { allows_multiple_answers: true, is_anonymous: false})
 }
